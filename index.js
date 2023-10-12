46deefd885c66051391ff542f792d9500efb06ab
sudhanshusingh2401wfx <140286811+sudhanshusingh2401wfx@users.noreply.github.com>
2023-10-13 00:17:22 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('It is working'); //write a response to the client
-  res.end(); //end the response
-}).listen(3000); //the server object listens on port 3000
